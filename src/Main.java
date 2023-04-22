da2fe9ba3c0b6bcac6bc613b4880f493f6448613
adhoc90 <ad_hoc90@mail.ru>
2023-04-22 20:46:40 +0300

    mission complete

--- src/Main.java
+++ src/Main.java
@@ -16,12 +16,13 @@ public class Main {
 
     public static void task2() {
         String fullName = "Ivanov Ivan Ivanovich";
-        System.out.println(fullName.toUpperCase());
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " +
+                fullName.toUpperCase());
     }
 
     public static void task3() {
         String fullName = "Иванов Семён Семёнович";
         String s1 = fullName.replace("ё", "е");
-        System.out.println(s1);
+        System.out.println("Данные ФИО сотрудника — " + s1);
     }
 }
